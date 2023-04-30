6ebd57954861323be159b3c322401ff6015ce0b3
mammadli <gulnar.mammadli@ut.ee>
2023-04-30 17:52:15 +0300

    Add createLoan and getLoan endpoints

--- src/main/java/com/group11/msloan/controller/LoanController.java
+++ src/main/java/com/group11/msloan/controller/LoanController.java
@@ -1,10 +1,10 @@
 package com.group11.msloan.controller;
 
+import com.group11.msloan.model.Loan;
+import com.group11.msloan.model.dto.LoanCreateDto;
 import com.group11.msloan.service.LoanService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/loans")
@@ -13,6 +13,13 @@ public class LoanController {
 
     private final LoanService loanService;
 
-//    @PostMapping
+    @PostMapping
+    Loan createLoan(@RequestBody LoanCreateDto loanCreateDto){
+        return loanService.createLoan(loanCreateDto);
+    }
 
+    @GetMapping("/customerId/documentId/{customerId}/{documentId}")
+    Loan getLoan(@PathVariable Long customerId, @PathVariable Long documentId){
+        return loanService.getLoan(customerId,documentId);
+    }
 }
--- src/main/java/com/group11/msloan/service/LoanService.java
+++ src/main/java/com/group11/msloan/service/LoanService.java
@@ -1,12 +1,36 @@
 package com.group11.msloan.service;
 
+import com.group11.msloan.mapper.LoanMapper;
+import com.group11.msloan.model.Loan;
+import com.group11.msloan.model.dto.LoanCreateDto;
+import com.group11.msloan.model.enums.Status;
 import com.group11.msloan.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LoanService {
 
     private final LoanRepository loanRepository;
+
+    public Loan createLoan(LoanCreateDto loanDto) {
+
+        Optional<Loan> result = loanRepository.findByCustomerIdAndDocumentId(loanDto.getCustomerId(), loanDto.getDocumentId());
+        if (result.isEmpty()) {
+            Loan loan = LoanMapper.INSTANCE.mapToCustomer(loanDto);
+            loan.setCreatedAt(LocalDateTime.now());
+            loan.setStatus(Status.INITIALIZED);
+            return loanRepository.save(loan);
+        }
+        return null;
+    }
+
+    public Loan getLoan(Long customerId, Long documentId) {
+        Optional<Loan> loan = loanRepository.findByCustomerIdAndDocumentId(customerId, documentId);
+        return loan.orElse(null);
+    }
 }
