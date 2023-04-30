291e101033e05ebd5668388a467896b782f261d8
mammadli <gulnar.mammadli@ut.ee>
2023-04-30 17:24:34 +0300

    Add mapper

---
+++ src/main/java/com/group11/msloan/mapper/LoanMapper.java
@@ -0,0 +1,16 @@
+package com.group11.msloan.mapper;
+
+
+import com.group11.msloan.model.Loan;
+import com.group11.msloan.model.dto.LoanDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface LoanMapper {
+
+    LoanMapper INSTANCE = Mappers.getMapper(LoanMapper.class);
+    Loan mapToCustomer(LoanDto loanDto);
+}
+
+
