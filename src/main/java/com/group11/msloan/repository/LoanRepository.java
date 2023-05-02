278cdeeeb78f94f86a103e69e2caa706479d0742
mammadli <gulnar.mammadli@ut.ee>
2023-05-02 14:06:18 +0300

    Add new queries

--- src/main/java/com/group11/msloan/repository/LoanRepository.java
+++ src/main/java/com/group11/msloan/repository/LoanRepository.java
@@ -1,12 +1,26 @@
 package com.group11.msloan.repository;
 
 import com.group11.msloan.model.Loan;
+import com.group11.msloan.model.enums.LoanType;
+import com.group11.msloan.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface LoanRepository  extends JpaRepository<Loan,Long> {
-Optional<Loan> findByCustomerIdAndDocumentId(Long customerId, Long DocumentId);
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    Optional<List<Loan>> findByCustomerIdAndLoanType(Long customerId, LoanType loanType);
+
+    Optional<Loan> findAllByCustomerIdAndId(Long customerId,Long id);
+
+    Optional<List<Loan>> findAllByCustomerIdOrStatusOrLoanTypeOrAmountLessThanOrAmountGreaterThan(Long customerId,
+                                                                                                  Status status,
+                                                                                                  LoanType loanType,
+                                                                                                  BigDecimal amount,
+                                                                                                  BigDecimal quantity);
 }
+
